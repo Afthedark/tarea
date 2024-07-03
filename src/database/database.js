f19d2c812c32cbe94926b943eff490c7f694f52d
Luis Gonzalo <afthedark@hotmail.com>
2024-07-02 22:21:06 -0400

    fix data base

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 
 import { Sequelize } from "sequelize"
 import 'dotenv/config'
-import { Status } from "../constants"
+import { Status } from "../constants/index.js"
 
 export const sequelize = new Sequelize(
     process.env.DB_DATABASE, //db name
